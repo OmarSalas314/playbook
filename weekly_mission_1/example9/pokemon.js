7695af08a9887ae9b9255ef7590b82372cc8a174
OmarSalas314 <omarsalasvieyra@gmail.com>
2022-04-16 03:12:54 -0600

    ""

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,10 @@
+class pokemon {
+    constructor(name) {
+      // this es una variable para referenciar el valor del contexto local de esta clase
+      this.name = name // Est�s variables se le conocen como atributos
+    }
+
+export class pokemon {
+    constructor (name) {
+      this.name = name
+}
